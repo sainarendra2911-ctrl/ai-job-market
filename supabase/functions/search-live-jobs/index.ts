5bfe625cd640274b00863d7db7afbf611dbb40e0
sainarendra2911-ctrl <sainarendra2911@gmail.com>
2026-07-18 22:45:01 +0530

    Updated index.ts

--- supabase/functions/search-live-jobs/index.ts
+++ supabase/functions/search-live-jobs/index.ts
@@ -6,6 +6,13 @@ const corsHeaders = {
   'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Client-Info, Apikey',
 };
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type",
+};
+
 Deno.serve(async (req: Request) => {
   if (req.method === 'OPTIONS') {
     return new Response(null, { status: 200, headers: corsHeaders });
